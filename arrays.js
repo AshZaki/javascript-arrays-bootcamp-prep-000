37e5f18fac0f5f4f3ed355c946f552767a64a180
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-07 02:46:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray(arr,element){
   return arr;
 }
 
-function accessElementInArray(arr,element){
+function accessElementInArray(arr,index){
    (index[0] + element);
   return index;
 }
