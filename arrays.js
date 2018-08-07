d150b17c926ce16a97b430a7c42fa5820a974a93
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-07 02:35:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(arr,element){
+  arr = [element, ...arr];
+  return arr;
+}
+
+function destructivelyAddElementToBeginningOfArray(arr,element){
+  arr.unshift(element);
+  return arr;
+}
+
+function addElementToEndOfArray(arr,element){
+  arr = [...arr,element];
+  return arr;
+}
+
+function destructivelyAddElementToEndOfArray(arr,element){
+  arr.push(element);
+  return arr;
+}
